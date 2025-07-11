dff6a9ff041286994968ad2b2d25eed600bc4d5f
Md. Abu Sayem <abusayem433@gmail.com>
2025-07-11 18:34:57 +0600

    Add Render domain to ALLOWED_HOSTS

--- pathfinder/settings.py
+++ pathfinder/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-your-secret-key-here'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'pathfinder-2hcb.onrender.com']
 
 # Application definition
 INSTALLED_APPS = [
